e4ce8d615e0e2ed702ea1d444ec873704a176038
erick <erickwelberth1@hotmail.com>
2025-05-09 19:28:11 -0300

    feat[bbd]: dare-1

---
+++ src/dare1/Dare1.java
@@ -0,0 +1,8 @@
+package dare1;
+
+public class Dare1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
