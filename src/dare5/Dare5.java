384085d8fdbb1635fd81f155681114a80cada7ba
erick <erickwelberth1@hotmail.com>
2025-05-25 15:42:57 -0300

    feat: [easy] Int to String

---
+++ src/dare5/Dare5.java
@@ -0,0 +1,6 @@
+package dare5;
+
+public class Dare5 {
+    //the hackerranck platform provided the entire code structure. The challenge consists only of converting an integer into a string.
+    String s = Integer.toString(n);
+}
