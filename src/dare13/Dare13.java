324d62ea5cedaddcaa3a7d45b961c737de1c98be
erick <erickwelberth1@hotmail.com>
2025-05-31 19:54:56 -0300

    feat: [easy] Static Initializer Block

---
+++ src/dare13/Dare13.java
@@ -0,0 +1,30 @@
+package dare13;
+
+import java.util.Scanner;
+
+public class Dare13 {
+
+    static int B;
+    static int H;
+    static boolean flag = true;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+
+        if (B <= 0 || H <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args){
+        if(flag){
+            int area = B * H;
+            System.out.print(area);
+        }
+
+    }
+
+}
